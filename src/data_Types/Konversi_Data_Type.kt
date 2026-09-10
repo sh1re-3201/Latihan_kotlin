d294550d7aa9644df4da4c158e983cfe58af0303
Anurgah <anugrahmapia@gmail.com>
2026-09-10 15:19:48 +0700

    Data type convert Done

--- src/data_Types/Konversi_Data_Type.kt
+++ src/data_Types/Konversi_Data_Type.kt
@@ -1,1 +1,13 @@
 package data_Types
+
+fun main() {
+    println("KONVERSI TIPE DATA")
+    print("Masukkan bilangan desimal: ")
+    val bilangan = readln()
+    val retconDouble: Double = bilangan.toDouble()
+    val retconInt = retconDouble.toInt()
+
+    println("Nilai Double: $retconDouble \n" +
+            "Nilai Int: $retconInt \n" +
+            "Nilai String: $bilangan")
+}
