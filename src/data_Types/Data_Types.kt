c24d947fed6697fbb296152a5f86f6dc43763bdb
Anurgah <anugrahmapia@gmail.com>
2026-09-10 15:02:19 +0700

    All files constructed + Data Types task done

---
+++ src/data_Types/Data_Types.kt
@@ -0,0 +1,15 @@
+package data_Types
+
+fun main(){
+    val nama = "Salomo"
+    val umur = 40
+    val tinggi = 165.43
+    val statusAktif = true
+    val golonganDarah = 'B'
+
+    println("Nama: $nama \n" +
+            "Umur: $umur \n" +
+            "Tinggi: $tinggi \n" +
+            "Mahasiswa Aktif: $statusAktif \n" +
+            "Golongan Darah: $golonganDarah")
+}
