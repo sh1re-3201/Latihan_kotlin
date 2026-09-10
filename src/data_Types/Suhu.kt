8513e4cc8ca9f5d2883794ed2f7bdd9deaed661e
Anurgah <anugrahmapia@gmail.com>
2026-09-10 15:14:14 +0700

    Temp Convert Done

--- src/data_Types/Suhu.kt
+++ src/data_Types/Suhu.kt
@@ -1,1 +1,16 @@
 package data_Types
+import java.util.Scanner
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+    println("KALKULATOR SUHU")
+    print("Masukkan suhu dalam Celcius: ")
+    val suhu = scanner.nextDouble()
+
+    val fahrenheit: Double = (suhu * 9/5) +32
+    val kelvin: Double = suhu + 273.15
+
+    println("Suhu Celsius:        $suhu \n" +
+            "Suhu Fahrenheit:     $fahrenheit \n" +
+            "Suhu Kelvin:         $kelvin")
+}
