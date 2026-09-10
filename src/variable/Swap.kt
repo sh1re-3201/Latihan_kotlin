cae999c7486fa7e3dac985cf142fb337244e3950
Anurgah <anugrahmapia@gmail.com>
2026-09-10 15:59:02 +0700

    Swapped bro Done

--- src/variable/Swap.kt
+++ src/variable/Swap.kt
@@ -1,2 +1,16 @@
 package variable
 
+fun main() {
+    println("MENUKAR INPUT")
+    print("Masukkan input pertama: ")
+    var input1 = readln()
+    print("Menukarkan input kedua: ")
+    var input2 = readln()
+
+    val temp = input1
+    input1 = input2
+    input2 = temp
+    println("HASIL MENUKAR INPUT\n" +
+            "input1: $input1\n" +
+            "input2: $input2")
+}
