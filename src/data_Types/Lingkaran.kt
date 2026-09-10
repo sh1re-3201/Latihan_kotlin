f7f24ac0bb2bef4b5ddd54432daf3e48c9550c44
Anurgah <anugrahmapia@gmail.com>
2026-09-10 15:26:01 +0700

    Lingkaran sudah melingkar Done

--- src/data_Types/Lingkaran.kt
+++ src/data_Types/Lingkaran.kt
@@ -1,1 +1,17 @@
 package data_Types
+import java.util.Scanner
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+    println("LUAS DAN KELILING LINGKARAN")
+    val pi: Double = 3.14159
+
+    print("Masukkan jari-jari lingkaran anda: ")
+    val r = scanner.nextDouble()
+
+    val luas = pi * (2*r)
+    val keliling = 2 * pi * r
+
+    println("Hasil Luas: $luas \n" +
+            "Hasil Keliling: $keliling")
+}
