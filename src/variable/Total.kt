c7983b3e6633e901a61858e4a421ddbe026b9d46
Anurgah <anugrahmapia@gmail.com>
2026-09-10 16:07:18 +0700

    Done Shopping

--- src/variable/Total.kt
+++ src/variable/Total.kt
@@ -1,2 +1,21 @@
 package variable
 
+fun main() {
+    val namaBarang = "Buku"
+    val hargaBarang = 15000.0
+    val jumlahBarang = 3
+    val diskon = 10.0
+
+    val subtotal = hargaBarang * jumlahBarang
+    val potongan = subtotal * diskon / 100
+    val totalBayar = subtotal - potongan
+
+    println("=== TOTAL BELANJA ===")
+    println("Nama barang   : $namaBarang")
+    println("Harga barang  : Rp$hargaBarang")
+    println("Jumlah barang : $jumlahBarang")
+    println("Diskon        : $diskon%")
+    println("Subtotal      : Rp$subtotal")
+    println("Potongan      : Rp$potongan")
+    println("Total bayar   : Rp$totalBayar")
+}
