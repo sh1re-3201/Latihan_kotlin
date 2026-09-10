42167a07218cbf332b3844bfb779569712340ca7
Anurgah <anugrahmapia@gmail.com>
2026-09-10 15:38:25 +0700

    Ternyata A bukanlah angka Done

--- src/data_Types/Karakter.kt
+++ src/data_Types/Karakter.kt
@@ -1,1 +1,20 @@
 package data_Types
+import java.util.Scanner
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+
+    println("ANALISIS KARAKTER")
+
+    print("Masukkan karakter: ")
+    val karakter: Char = scanner.next().first()
+
+    val asciiCode = karakter.code
+    val isLetter = karakter.isLetter()
+    val isNumber = karakter.isDigit()
+
+    println("Karakter: $karakter \n" +
+            "Kode ASCII: $asciiCode \n" +
+            "Apakah Huruf: $isLetter \n" +
+            "Apakah Angka: $isNumber")
+}
